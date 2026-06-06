64f2fcbd8df6e29979b9565711b1b5b9e3b73c0f
ez-coder13 <amritvirrandhawa@gmail.com>
2026-06-05 21:05:18 -0700

    Build login page

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,42 @@
+import "./Login.scss";
+import logo from "../../assets/images/logo.png";
+import loginIllustration from "../../assets/images/pablo-sign-in 1.svg";
+
+const Login = () => {
+  return (
+    <main className="login">
+      <section className="login__left">
+        <img src={logo} alt="Lendsqr logo" className="login__logo" />
+        <img
+          src={loginIllustration}
+          alt="Login illustration"
+          className="login__illustration"
+        />
+      </section>
+
+      <section className="login__right">
+        <form className="login__form">
+          <h1>Welcome!</h1>
+          <p>Enter details to login.</p>
+
+          <input type="email" placeholder="Email" aria-label="Email" />
+
+          <div className="login__password">
+            <input type="password" placeholder="Password" aria-label="Password" />
+            <button type="button">SHOW</button>
+          </div>
+
+          <a href="#" className="login__forgot">
+            FORGOT PASSWORD?
+          </a>
+
+          <button type="submit" className="login__button">
+            LOG IN
+          </button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default Login;
